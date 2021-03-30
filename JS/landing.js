6e7757866e5c57b062cc955d972083264f568e60
AnayGarodia <agarodia98@gmail.com>
2021-03-30 23:36:58 +0530

    Updated

--- JS/landing.js
+++ JS/landing.js
@@ -9,15 +9,55 @@ function showContent() {
         }
 
         showMoreLink.innerHTML = "View Less";
-    }
-
-    else {
+    } else {
         for (let i = 3; i < invisibleContentBox.length; i++) {
             invisibleContentBox[i].classList.add("d-none");
         }
 
         showMoreLink.innerHTML = "View More";
     }
-
-
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    const boxes = document.querySelectorAll(".third-part-content-box-container");
+    const circles = document.querySelectorAll(".circle-container");
+    circleAppear(circles[0]);
+    boxAppear(boxes[0])
+    document.addEventListener("scroll", () => {
+        circleAppear(circles[0]);
+        boxAppear(boxes[0])
+    });
+})
+
+function circleAppear(n) {
+    const circles = document.querySelectorAll(".circle-container");
+    if (window.pageYOffset > distanceFromTop(n) / 2) {
+        for (let i = 0; i < circles.length; i++) {
+            circles[i].style.animation = '1s appear'
+            circles[i].style.opacity = '1'
+        }
+    }
+}
+
+function boxAppear(n) {
+    const boxes = document.querySelectorAll(".third-part-content-box-container");
+    console.log(window.pageYOffset > distanceFromTop(n) / 1.25)
+    if (window.pageYOffset > distanceFromTop(n) / 1.25) {
+        for (let j = 0; j < boxes.length; j++) {
+            console.log('done')
+            boxes[j].style.animation = '1s appear'
+            boxes[j].style.opacity = '1'
+        }
+    }
+}
+
+function distanceFromTop(elem) {
+    var location = 0;
+    if (elem.offsetParent) {
+        do {
+            location += elem.offsetTop;
+            elem = elem.offsetParent;
+        } while (elem);
+    }
+    return location >= 0 ? location : 0;
+};
